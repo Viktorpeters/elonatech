3492a5e9fd11b98d4e2fc0e759b81ccb354a8b96
viktor ajayi <victorajayidamilare@gmail.com>
2025-04-29 13:38:48 +0100

    feat: created token

---
+++ question_two/utils/create_token.js
@@ -0,0 +1,26 @@
+import jwt from "jsonwebtoken";
+import { config } from "dotenv";
+
+// the token type represents the type of the token , whther access token or refresh token ,
+// the time should be put in seconds
+// e.g 15minutes
+export const generateToken = (userId, tokenType, time) => {
+  const secret =
+    tokenType === "ACCESS"
+      ? process.env.YOUR_ACCESS_TOKEN_SECRET
+      : process.env.YOUR_REFRESH_TOKEN_SECRET;
+  const expiresIn =
+    tokenType === "ACCESS"
+      ? process.env.YOUR_ACCESS_TOKEN_EXPIRE_TIME
+      : process.env.YOUR_REFRESH_TOKEN_EXPIRE_TIME;
+
+  if (tokenType === "ACCESS") {
+    return jwt.sign({ userId }, secret, {
+      expiresIn: expiresIn,
+    });
+  } else if (tokenType === "REFRESH") {
+    return jwt.sign({ userId }, secret, {
+      expiresIn: expiresIn,
+    });
+  } else return null;
+};
